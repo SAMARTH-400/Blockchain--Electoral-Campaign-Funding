81e498c7429543d9a86c125a1d47d8dec8690916
umeshkaushik21 <umesh.k@beaconstac.com>
2023-06-06 01:54:41 +0530

    minor changes

---
+++ Components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  const productList = ["Market", "ERC20 Token", "Donation"];
+  const contactList = [
+    "support@cryptoking.com",
+    "info@example.com",
+    "Contact us",
+  ];
+  const usefullLink = ["Home", "About Us", "Company Bio"];
+  return (
+    <footer class="text-center text-white backgroundMain lg:text-left">
+      <div class="mx-6 py-10 text-center md:text-left h-56">
+      </div>
+     
+    </footer>
+  );
+};
+
+export default Footer;
